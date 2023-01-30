f687b70c014b2236c22d1d8305f0abecb089c31f
HondDK <Honddk99@gmail.com>
2023-01-30 22:10:01 +0600

    testValidate

--- src/components/UI/ButtonCloseTest/ButtonCloseTest.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ButtonCloseTest = (props) => {
-	return <a >{props.children}</a>;
-};
-
-export default ButtonCloseTest;
--- src/pages/TestOne.js
+++ src/pages/TestOne.js
@@ -5,8 +5,31 @@ import { useNavigate } from "react-router-dom";
 
 const TestOne = () => {
 	let navigate = useNavigate();
+	const correctOptions = [1, 5, 6, 7];
+
 	const [value, setValue] = useState();
 	const [min, setMin] = useState(2);
+	const [selectedOption, setSelectedOption] = useState("");
+	const [isCorrect, setIsCorrect] = useState(false);
+	const [selectedButtons, setSelectedButtons] = useState([]);
+
+	const handleButtonClick = (button) => {
+		setSelectedOption((prevOptions) => {
+			if (prevOptions.includes(button)) {
+				return prevOptions.filter((o) => o !== button);
+			} else {
+				return [...prevOptions, button];
+			}
+		});
+
+		if (selectedButtons.includes(button)) {
+			setSelectedButtons(
+				selectedButtons.filter((selected) => selected !== button)
+			);
+		} else if (selectedButtons.length < 4) {
+			setSelectedButtons([...selectedButtons, button]);
+		}
+	};
 
 	const handleChange = (value) => {
 		setValue(value);
@@ -14,9 +37,14 @@ const TestOne = () => {
 			navigate("/testtwo", { replace: true });
 		}
 	};
-	const buttonSubmit = () => {
-		console.log(value);
-		navigate("/testtwo", { replace: true });
+
+	const buttonSubmit = (e) => {
+		e.preventDefault();
+		const correctCount = selectedOption.filter((option) =>
+			correctOptions.includes(option)
+		).length;
+		console.log(`правильных ${correctCount}`);
+		//navigate("/testtwo", { replace: true });
 	};
 
 	return (
@@ -24,7 +52,7 @@ const TestOne = () => {
 			<body class="test">
 				<header>
 					<h1>Практическое задание №1</h1>
-					<Time min={min} onChange={handleChange} />
+					{/* <Time min={min} onChange={handleChange} /> */}
 				</header>
 				<main class="main__test">
 					<div class="test-title">
@@ -37,17 +65,80 @@ const TestOne = () => {
 							Необходимо разработать комплекс мер по замедлению или прекращению
 							спада производства и снижению темпов инфляции
 						</p>
-						<ButtonReply></ButtonReply>
+						<div>
+							<button
+								style={{
+									backgroundColor: selectedButtons.includes(1)
+										? "green"
+										: "white",
+								}}
+								onClick={() => handleButtonClick(1)}
+							>
+								Нормативная
+							</button>
+							<button
+								style={{
+									backgroundColor: selectedButtons.includes(2)
+										? "green"
+										: "white",
+								}}
+								onClick={() => handleButtonClick(2)}
+							>
+								Позитивная
+							</button>
+						</div>
 						<p class="p__test-text">
 							За последнее время в экономике страны наблюдается спад
 							производства и высокие темпы инфляции
 						</p>
-						<ButtonReply></ButtonReply>
+						<div>
+							<button
+								style={{
+									backgroundColor: selectedButtons.includes(3)
+										? "green"
+										: "white",
+								}}
+								onClick={() => handleButtonClick(3)}
+							>
+								Нормативная
+							</button>
+							<button
+								style={{
+									backgroundColor: selectedButtons.includes(4)
+										? "green"
+										: "white",
+								}}
+								onClick={() => handleButtonClick(4)}
+							>
+								Позитивная
+							</button>
+						</div>
 						<p class="p__test-text">
 							Структурная перестройка экономики страны, помимо других целей,
 							направлена на повышение уровня потребления людей
 						</p>
-						<ButtonReply></ButtonReply>
+						<div>
+							<button
+								style={{
+									backgroundColor: selectedButtons.includes(5)
+										? "green"
+										: "white",
+								}}
+								onClick={() => handleButtonClick(5)}
+							>
+								Нормативная
+							</button>
+							<button
+								style={{
+									backgroundColor: selectedButtons.includes(6)
+										? "green"
+										: "white",
+								}}
+								onClick={() => handleButtonClick(6)}
+							>
+								Позитивная
+							</button>
+						</div>
 						<p class="p__test-text">
 							Осуществление структурной перестройки неизбежно ведет к разрушению
 							эгалитаризма, господствующего в мышлении многих людей, резкому
@@ -55,7 +146,28 @@ const TestOne = () => {
 							дестабилизации, и поэтому структурная перестройка должна
 							осуществляться постепенно
 						</p>
-						<ButtonReply></ButtonReply>
+						<div>
+							<button
+								style={{
+									backgroundColor: selectedButtons.includes(7)
+										? "green"
+										: "white",
+								}}
+								onClick={() => handleButtonClick(7)}
+							>
+								Нормативная
+							</button>
+							<button
+								style={{
+									backgroundColor: selectedButtons.includes(8)
+										? "green"
+										: "white",
+								}}
+								onClick={() => handleButtonClick(8)}
+							>
+								Позитивная
+							</button>
+						</div>
 					</div>
 
 					<button className="btn__close-task" onClick={buttonSubmit}>
