9fe57e3d3d5916ca430acca5ab20ab7333fecab2
HondDK <Honddk99@gmail.com>
2023-01-31 00:29:48 +0600

    textThreeValidate

--- src/pages/TestThree.js
+++ src/pages/TestThree.js
@@ -4,19 +4,36 @@ import { useNavigate } from "react-router-dom";
 
 const TestThree = () => {
 	let navigate = useNavigate();
-
-	const buttonSubmit = () => {
-		navigate("/testfour", { replace: true });
-	};
+	const correctValues = [1, 2, 4, 6, 3, 5, 7];
 
 	const [value, setValue] = useState();
 	const [min, setMin] = useState(9);
+
+	const [value1, setValue1] = useState("");
+	const [value2, setValue2] = useState("");
+	const [value3, setValue3] = useState("");
+	const [value4, setValue4] = useState("");
+	const [value5, setValue5] = useState("");
+	const [value6, setValue6] = useState("");
+	const [value7, setValue7] = useState("");
 
 	const handleChange = (value) => {
 		setValue(value);
 		if (value == false) {
 			navigate("/testfour", { replace: true });
 		}
+	};
+
+	const buttonSubmit = (e) => {
+		e.preventDefault();
+		let correctCount = 0;
+		const values = [value1, value2, value3, value4, value5, value6, value7];
+
+		for (let i = 0; i < values.length; i++) {
+			if (parseInt(values[i]) === correctValues[i]) correctCount++;
+		}
+		console.log(correctCount);
+		navigate("/testfour", { replace: true });
 	};
 
 	return (
@@ -69,18 +86,46 @@ const TestThree = () => {
 						<div class="container__button">
 							<div class="container__box">Натуральное хозяйство</div>
 							<div class="container__inputs">
-								<input type="text" />
-								<input type="text" />
-								<input type="text" />
-								<input type="text" />
+								<input
+									type="text"
+									value={value1}
+									onChange={(e) => setValue1(e.target.value)}
+								/>
+								<input
+									type="text"
+									value={value2}
+									onChange={(e) => setValue2(e.target.value)}
+								/>
+								<input
+									type="text"
+									value={value3}
+									onChange={(e) => setValue3(e.target.value)}
+								/>
+								<input
+									type="text"
+									value={value4}
+									onChange={(e) => setValue4(e.target.value)}
+								/>
 							</div>
 						</div>
 						<div class="container__button">
 							<div class="container__box">Товарное производство </div>
 							<div class="container__inputs">
-								<input type="text" />
-								<input type="text" />
-								<input type="text" />
+								<input
+									type="text"
+									value={value5}
+									onChange={(e) => setValue5(e.target.value)}
+								/>
+								<input
+									type="text"
+									value={value6}
+									onChange={(e) => setValue6(e.target.value)}
+								/>
+								<input
+									type="text"
+									value={value7}
+									onChange={(e) => setValue7(e.target.value)}
+								/>
 							</div>
 						</div>
 					</div>
