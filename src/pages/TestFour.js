f1082440459740680f41b8f99646dab07b3f3702
HondDK <Honddk99@gmail.com>
2023-02-02 17:35:36 +0600

    testFourTable

--- src/pages/TestFour.js
+++ src/pages/TestFour.js
@@ -34,73 +34,131 @@ const TestFour = () => {
 				</header>
 
 				<main class="main__test">
-					<div class="text">
-						Для каждого из приведенных здесь понятий и терминов подберите
-						соответствующее ему определение:
-					</div>
-					<div class="blocks">
-						<div class="block__text">Cпрос</div>
-						<div class="block__desk">
-							Товары, которые потребляются совместно; при этом один без другого,
-							как правило, не может быть использован к потреблению
-						</div>
-						<div class="block__text">Величина спроса</div>
-						<div class="block__desk">
-							График (или шкала), показывающий, сколько единиц товара покупатели
-							хотят и могут купить во всем диапазоне цен в данный момент времени
-						</div>
-						<div class="block__text">Закон спроса</div>
-						<div class="block__desk">
-							Факторы, вызывающие изменение спроса и сдвиг кривой спроса
-						</div>
-						<div class="block__text">Эффект дохода</div>
-						<div class="block__desk">
-							Товары, которые могут быть использованы вместо других товаров с
-							аналогичными свойствами
-						</div>
-						<div class="block__text">Эффект замещения</div>
-						<div class="block__desk">
-							Товары, спрос на которые падает по мере роста доходов потребителей
-						</div>
-						<div class="block__text">
-							Принцип убывания маржинальной полезности
-						</div>
-						<div class="block__desk">
-							Количество товара, которое покупатели хотят и могут приобрести при
-							данном уровне цены
-						</div>
-						<div class="block__text">Эффект Гиффена</div>
-						<div class="block__desk">
-							Явление, которое возникает, когда вследствие снижения цены товара
-							происходит относительное увеличение дохода покупателя, позволяющее
-							последнему приобрести большее количество товара
-						</div>
-						<div class="block__text">Детерминанты спроса</div>
-						<div class="block__desk">
-							Товары, спрос на которые растет по мере увеличения доходов
-							покупателей
-						</div>
-						<div class="block__text">Низшие товары</div>
-						<div class="block__desk">
-							С ростом потребления блага (при неизменном объёме потребления всех
-							остальных благ) общая полезность, получаемая потребителем,
-							возрастает, но скорость роста замедляется
-						</div>
-						<div class="block__text">Нормальные товары</div>
-						<div class="block__desk">
-							Закон, в соответствии с которым величина спроса на товар находится
-							в обратной зависимости от цены этого товара
-						</div>
-						<div class="block__text">Товары-субституты</div>
-						<div class="block__desk">
-							Явление, состоящее в том, что повышение цены товара ведет к росту
-							объема спроса на него
-						</div>
-						<div class="block__text">Товары-комплементы</div>
-						<div class="block__desk">
-							Явление, которое возникает, когда покупатель отдает предпочтение
-							более дешевому товару, вытесняющему при выборе покупки товар,
-							ставший относительно дороже
+					<div
+						class="divTable"
+						style={{ width: "100%", border: "1px solid #000" }}
+					>
+						<div class="divTableBody">
+							<div class="divTableRow">
+								<div class="divTableCell">
+									<span style={{ fontWeight: "400" }}>Спрос</span>&nbsp;
+								</div>
+								<div class="divTableCell">
+									Товары, которые потребляются совместно; при этом один без
+									другого, как правило, не может быть использован к
+									потреблению&nbsp;
+								</div>
+							</div>
+							<div class="divTableRow">
+								<div class="divTableCell">
+									<span style={{ fontWeight: "400" }}>Величина спроса</span>
+								</div>
+								<div class="divTableCell">
+									{" "}
+									График (или шкала), показывающий, сколько единиц товара
+									покупатели хотят и могут купить во всем диапазоне цен в данный
+									момент времени&nbsp;
+								</div>
+							</div>
+							<div class="divTableRow">
+								<div class="divTableCell">
+									<span style={{ fontWeight: "400" }}>Закон спроса</span>
+								</div>
+								<div class="divTableCell">
+									Факторы, вызывающие изменение спроса и сдвиг кривой
+									спроса&nbsp;
+								</div>
+							</div>
+							<div class="divTableRow">
+								<div class="divTableCell">
+									<span style={{ fontWeight: "400" }}>Эффект дохода</span>
+								</div>
+								<div class="divTableCell">
+									Товары, которые могут быть использованы вместо других товаров
+									с аналогичными свойствами &nbsp;
+								</div>
+							</div>
+							<div class="divTableRow">
+								<div class="divTableCell">
+									<span style={{ fontWeight: "400" }}>Эффект замещения</span>
+								</div>
+								<div class="divTableCell">
+									Количество товара, которое покупатели хотят и могут приобрести
+									при данном уровне цены &nbsp;
+								</div>
+							</div>
+							<div class="divTableRow">
+								<div class="divTableCell">
+									<span style={{ fontWeight: "400" }}>
+										Принцип убывания маржинальной полезности
+									</span>
+								</div>
+								<div class="divTableCell">
+									Явление, которое возникает, когда вследствие снижения цены
+									товара происходит относительное увеличение дохода покупателя,
+									позволяющее последнему приобрести большее количество
+									товара&nbsp;
+								</div>
+							</div>
+							<div class="divTableRow">
+								<div class="divTableCell">
+									<span style={{ fontWeight: "400" }}>Эффект Гиффена</span>
+								</div>
+								<div class="divTableCell">
+									Товары, спрос на которые растет по мере увеличения доходов
+									покупателей&nbsp;
+								</div>
+							</div>
+							<div class="divTableRow">
+								<div class="divTableCell">
+									<span style={{ fontWeight: "400" }}>Детерминанты спроса</span>
+								</div>
+								<div class="divTableCell">
+									с ростом потребления блага (при неизменном объёме потребления
+									всех остальных благ) общая полезность, получаемая
+									потребителем, возрастает, но скорость роста замедляется&nbsp;
+								</div>
+							</div>
+							<div class="divTableRow">
+								<div class="divTableCell">
+									<span style={{ fontWeight: "400" }}>Низшие товары</span>
+								</div>
+								<div class="divTableCell">
+									<span style={{ fontWeight: "400" }}>
+										Закон, в соответствии с которым величина спроса на товар
+										находится в обратной зависимости от цены этого товара
+									</span>
+									&nbsp;
+								</div>
+							</div>
+							<div class="divTableRow">
+								<div class="divTableCell">
+									<span style={{ fontWeight: "400" }}>Нормальные товары</span>
+								</div>
+								<div class="divTableCell">
+									Явление, состоящее в том, что повышение цены товара ведет к
+									росту объема спроса на него&nbsp;
+								</div>
+							</div>
+							<div class="divTableRow">
+								<div class="divTableCell">
+									<span style={{ fontWeight: "400" }}>Товары-субституты</span>
+								</div>
+								<div class="divTableCell">
+									Явление, которое возникает, когда покупатель отдает
+									предпочтение более дешевому товару, вытесняющему при выборе
+									покупки товар, ставший относительно дороже&nbsp;
+								</div>
+							</div>
+							<div class="divTableRow">
+								<div class="divTableCell">
+									<span style={{ fontWeight: "400" }}>Товары-комплементы</span>
+								</div>
+								<div class="divTableCell">
+									Товары, спрос на которые падает по мере роста доходов
+									потребителей&nbsp;
+								</div>
+							</div>
 						</div>
 					</div>
 				</main>
