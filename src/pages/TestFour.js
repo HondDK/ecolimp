321355bc5682809d2a926af8c71e873c4576c0c9
HondDK <Honddk99@gmail.com>
2023-02-02 21:50:58 +0600

    card

--- src/pages/TestFour.js
+++ src/pages/TestFour.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import Time from "../components/UI/Time/Time";
 import { useNavigate } from "react-router-dom";
 import { removeSessionStorage } from "../libs/helpers/removeSessionStorage";
+import { createRenderer } from "react-dom/test-utils";
 
 const TestFour = () => {
 	const [value, setValue] = useState();
@@ -23,6 +24,105 @@ const TestFour = () => {
 
 		navigate("/testfive", { replace: true });
 	};
+
+	const [card, setCard] = useState([
+		{
+			id: 1,
+			order: 12,
+			text: "Товары, которые потребляются совместно; при этом один без другого, как правило, не может быть использован к потреблению",
+		},
+		{
+			id: 2,
+			order: 2,
+			text: "График (или шкала), показывающий, сколько единиц товара покупатели хотят и могут купить во всем диапазоне цен в данный момент времени",
+		},
+		{
+			id: 3,
+			order: 8,
+			text: "Факторы, вызывающие изменение спроса и сдвиг кривой спроса",
+		},
+		{
+			id: 4,
+			order: 11,
+			text: "Товары, которые могут быть использованы вместо других товаров с аналогичными свойствами",
+		},
+		{
+			id: 5,
+			order: 9,
+			text: "Товары, спрос на которые падает по мере роста доходов потребителей",
+		},
+		{
+			id: 6,
+			order: 1,
+			text: "Количество товара, которое покупатели хотят и могут приобрести при данном уровне цены",
+		},
+		{
+			id: 7,
+			order: 4,
+			text: "Явление, которое возникает, когда вследствие снижения цены товара происходит относительное увеличение дохода покупателя, позволяющее последнему приобрести большее количество товара",
+		},
+		{
+			id: 8,
+			order: 10,
+			text: "Товары, спрос на которые растет по мере увеличения доходов покупателей",
+		},
+		{
+			id: 9,
+			order: 6,
+			text: "с ростом потребления блага (при неизменном объёме потребления всех остальных благ) общая полезность, получаемая потребителем, возрастает, но скорость роста замедляется",
+		},
+		{
+			id: 10,
+			order: 3,
+			text: "Закон, в соответствии с которым величина спроса на товар находится в обратной зависимости от цены этого товара",
+		},
+		{
+			id: 11,
+			order: 7,
+			text: "Явление, состоящее в том, что повышение цены товара ведет к росту объема спроса на него",
+		},
+		{
+			id: 12,
+			order: 5,
+			text: "Явление, которое возникает, когда покупатель отдает предпочтение более дешевому товару, вытесняющему при выборе покупки товар, ставший относительно дороже",
+		},
+	]);
+
+	const [currentCard, setCurrentCard] = useState(null);
+
+	const dragStartHandle = (e, set) => {
+		console.log("drag", set);
+		setCurrentCard(set);
+	};
+	const dragEndHandle = (e) => {};
+	const dragOverHandle = (e) => {
+		e.preventDefault();
+	};
+
+	const dropHandler = (e, set) => {
+		e.preventDefault();
+
+		setCard(
+			card.map((c) => {
+				if (c.id === set.id) {
+					return { ...c, order: currentCard.order };
+				}
+				if (c.id === currentCard.id) {
+					return { ...c, order: set.order };
+				}
+				return c;
+			})
+		);
+		console.log("drop", card);
+	};
+	const sortCard = (a, b) => {
+		if (a.order > b.order) {
+			return 1;
+		} else {
+			return -1;
+		}
+	};
+
 	return (
 		<div>
 			<body class="test">
@@ -36,17 +136,29 @@ const TestFour = () => {
 				<main class="main__test">
 					<div
 						class="divTable"
-						style={{ width: "100%", border: "1px solid #000" }}
+						style={{ width: "100%", border: "1px solid #000", height: "600px" }}
 					>
 						<div class="divTableBody">
 							<div class="divTableRow">
+								<div class="divTableCell">Cпрос</div>
 								<div class="divTableCell">
-									<span style={{ fontWeight: "400" }}>Спрос</span>&nbsp;
-								</div>
-								<div class="divTableCell">
-									Товары, которые потребляются совместно; при этом один без
-									другого, как правило, не может быть использован к
-									потреблению&nbsp;
+									{card
+										.sort(sortCard)
+										.slice(0, 1)
+										.map((set) => (
+											<div
+												style={{ cursor: "grab" }}
+												onDragStart={(e) => dragStartHandle(e, set)}
+												onDragLeave={(e) => dragEndHandle(e)}
+												onDragEnd={(e) => dragEndHandle(e)}
+												onDragOver={(e) => dragOverHandle(e)}
+												onDrop={(e) => dropHandler(e, set)}
+												draggable={true}
+												key={set.id}
+											>
+												{set.text}
+											</div>
+										))}
 								</div>
 							</div>
 							<div class="divTableRow">
@@ -54,10 +166,23 @@ const TestFour = () => {
 									<span style={{ fontWeight: "400" }}>Величина спроса</span>
 								</div>
 								<div class="divTableCell">
-									{" "}
-									График (или шкала), показывающий, сколько единиц товара
-									покупатели хотят и могут купить во всем диапазоне цен в данный
-									момент времени&nbsp;
+									{card
+										.sort(sortCard)
+										.slice(1, 2)
+										.map((set) => (
+											<div
+												style={{ cursor: "grab" }}
+												onDragStart={(e) => dragStartHandle(e, set)}
+												onDragLeave={(e) => dragEndHandle(e)}
+												onDragEnd={(e) => dragEndHandle(e)}
+												onDragOver={(e) => dragOverHandle(e)}
+												onDrop={(e) => dropHandler(e, set)}
+												draggable={true}
+												key={set.id}
+											>
+												{set.text}
+											</div>
+										))}
 								</div>
 							</div>
 							<div class="divTableRow">
@@ -65,8 +190,23 @@ const TestFour = () => {
 									<span style={{ fontWeight: "400" }}>Закон спроса</span>
 								</div>
 								<div class="divTableCell">
-									Факторы, вызывающие изменение спроса и сдвиг кривой
-									спроса&nbsp;
+									{card
+										.sort(sortCard)
+										.slice(2, 3)
+										.map((set) => (
+											<div
+												style={{ cursor: "grab" }}
+												onDragStart={(e) => dragStartHandle(e, set)}
+												onDragLeave={(e) => dragEndHandle(e)}
+												onDragEnd={(e) => dragEndHandle(e)}
+												onDragOver={(e) => dragOverHandle(e)}
+												onDrop={(e) => dropHandler(e, set)}
+												draggable={true}
+												key={set.id}
+											>
+												{set.text}
+											</div>
+										))}
 								</div>
 							</div>
 							<div class="divTableRow">
@@ -74,8 +214,23 @@ const TestFour = () => {
 									<span style={{ fontWeight: "400" }}>Эффект дохода</span>
 								</div>
 								<div class="divTableCell">
-									Товары, которые могут быть использованы вместо других товаров
-									с аналогичными свойствами &nbsp;
+									{card
+										.sort(sortCard)
+										.slice(3, 4)
+										.map((set) => (
+											<div
+												style={{ cursor: "grab" }}
+												onDragStart={(e) => dragStartHandle(e, set)}
+												onDragLeave={(e) => dragEndHandle(e)}
+												onDragEnd={(e) => dragEndHandle(e)}
+												onDragOver={(e) => dragOverHandle(e)}
+												onDrop={(e) => dropHandler(e, set)}
+												draggable={true}
+												key={set.id}
+											>
+												{set.text}
+											</div>
+										))}
 								</div>
 							</div>
 							<div class="divTableRow">
@@ -83,8 +238,23 @@ const TestFour = () => {
 									<span style={{ fontWeight: "400" }}>Эффект замещения</span>
 								</div>
 								<div class="divTableCell">
-									Количество товара, которое покупатели хотят и могут приобрести
-									при данном уровне цены &nbsp;
+									{card
+										.sort(sortCard)
+										.slice(4, 5)
+										.map((set) => (
+											<div
+												style={{ cursor: "grab" }}
+												onDragStart={(e) => dragStartHandle(e, set)}
+												onDragLeave={(e) => dragEndHandle(e)}
+												onDragEnd={(e) => dragEndHandle(e)}
+												onDragOver={(e) => dragOverHandle(e)}
+												onDrop={(e) => dropHandler(e, set)}
+												draggable={true}
+												key={set.id}
+											>
+												{set.text}
+											</div>
+										))}
 								</div>
 							</div>
 							<div class="divTableRow">
@@ -94,10 +264,23 @@ const TestFour = () => {
 									</span>
 								</div>
 								<div class="divTableCell">
-									Явление, которое возникает, когда вследствие снижения цены
-									товара происходит относительное увеличение дохода покупателя,
-									позволяющее последнему приобрести большее количество
-									товара&nbsp;
+									{card
+										.sort(sortCard)
+										.slice(5, 6)
+										.map((set) => (
+											<div
+												style={{ cursor: "grab" }}
+												onDragStart={(e) => dragStartHandle(e, set)}
+												onDragLeave={(e) => dragEndHandle(e)}
+												onDragEnd={(e) => dragEndHandle(e)}
+												onDragOver={(e) => dragOverHandle(e)}
+												onDrop={(e) => dropHandler(e, set)}
+												draggable={true}
+												key={set.id}
+											>
+												{set.text}
+											</div>
+										))}
 								</div>
 							</div>
 							<div class="divTableRow">
@@ -105,8 +288,23 @@ const TestFour = () => {
 									<span style={{ fontWeight: "400" }}>Эффект Гиффена</span>
 								</div>
 								<div class="divTableCell">
-									Товары, спрос на которые растет по мере увеличения доходов
-									покупателей&nbsp;
+									{card
+										.sort(sortCard)
+										.slice(6, 7)
+										.map((set) => (
+											<div
+												style={{ cursor: "grab" }}
+												onDragStart={(e) => dragStartHandle(e, set)}
+												onDragLeave={(e) => dragEndHandle(e)}
+												onDragEnd={(e) => dragEndHandle(e)}
+												onDragOver={(e) => dragOverHandle(e)}
+												onDrop={(e) => dropHandler(e, set)}
+												draggable={true}
+												key={set.id}
+											>
+												{set.text}
+											</div>
+										))}
 								</div>
 							</div>
 							<div class="divTableRow">
@@ -114,9 +312,23 @@ const TestFour = () => {
 									<span style={{ fontWeight: "400" }}>Детерминанты спроса</span>
 								</div>
 								<div class="divTableCell">
-									с ростом потребления блага (при неизменном объёме потребления
-									всех остальных благ) общая полезность, получаемая
-									потребителем, возрастает, но скорость роста замедляется&nbsp;
+									{card
+										.sort(sortCard)
+										.slice(7, 8)
+										.map((set) => (
+											<div
+												style={{ cursor: "grab" }}
+												onDragStart={(e) => dragStartHandle(e, set)}
+												onDragLeave={(e) => dragEndHandle(e)}
+												onDragEnd={(e) => dragEndHandle(e)}
+												onDragOver={(e) => dragOverHandle(e)}
+												onDrop={(e) => dropHandler(e, set)}
+												draggable={true}
+												key={set.id}
+											>
+												{set.text}
+											</div>
+										))}
 								</div>
 							</div>
 							<div class="divTableRow">
@@ -124,10 +336,23 @@ const TestFour = () => {
 									<span style={{ fontWeight: "400" }}>Низшие товары</span>
 								</div>
 								<div class="divTableCell">
-									<span style={{ fontWeight: "400" }}>
-										Закон, в соответствии с которым величина спроса на товар
-										находится в обратной зависимости от цены этого товара
-									</span>
+									{card
+										.sort(sortCard)
+										.slice(8, 9)
+										.map((set) => (
+											<div
+												style={{ cursor: "grab" }}
+												onDragStart={(e) => dragStartHandle(e, set)}
+												onDragLeave={(e) => dragEndHandle(e)}
+												onDragEnd={(e) => dragEndHandle(e)}
+												onDragOver={(e) => dragOverHandle(e)}
+												onDrop={(e) => dropHandler(e, set)}
+												draggable={true}
+												key={set.id}
+											>
+												{set.text}
+											</div>
+										))}
 									&nbsp;
 								</div>
 							</div>
@@ -136,8 +361,23 @@ const TestFour = () => {
 									<span style={{ fontWeight: "400" }}>Нормальные товары</span>
 								</div>
 								<div class="divTableCell">
-									Явление, состоящее в том, что повышение цены товара ведет к
-									росту объема спроса на него&nbsp;
+									{card
+										.sort(sortCard)
+										.slice(9, 10)
+										.map((set) => (
+											<div
+												style={{ cursor: "grab" }}
+												onDragStart={(e) => dragStartHandle(e, set)}
+												onDragLeave={(e) => dragEndHandle(e)}
+												onDragEnd={(e) => dragEndHandle(e)}
+												onDragOver={(e) => dragOverHandle(e)}
+												onDrop={(e) => dropHandler(e, set)}
+												draggable={true}
+												key={set.id}
+											>
+												{set.text}
+											</div>
+										))}
 								</div>
 							</div>
 							<div class="divTableRow">
@@ -145,9 +385,23 @@ const TestFour = () => {
 									<span style={{ fontWeight: "400" }}>Товары-субституты</span>
 								</div>
 								<div class="divTableCell">
-									Явление, которое возникает, когда покупатель отдает
-									предпочтение более дешевому товару, вытесняющему при выборе
-									покупки товар, ставший относительно дороже&nbsp;
+									{card
+										.sort(sortCard)
+										.slice(10, 11)
+										.map((set) => (
+											<div
+												style={{ cursor: "grab" }}
+												onDragStart={(e) => dragStartHandle(e, set)}
+												onDragLeave={(e) => dragEndHandle(e)}
+												onDragEnd={(e) => dragEndHandle(e)}
+												onDragOver={(e) => dragOverHandle(e)}
+												onDrop={(e) => dropHandler(e, set)}
+												draggable={true}
+												key={set.id}
+											>
+												{set.text}
+											</div>
+										))}
 								</div>
 							</div>
 							<div class="divTableRow">
@@ -155,8 +409,23 @@ const TestFour = () => {
 									<span style={{ fontWeight: "400" }}>Товары-комплементы</span>
 								</div>
 								<div class="divTableCell">
-									Товары, спрос на которые падает по мере роста доходов
-									потребителей&nbsp;
+									{card
+										.sort(sortCard)
+										.slice(11, 12)
+										.map((set) => (
+											<div
+												style={{ cursor: "grab" }}
+												onDragStart={(e) => dragStartHandle(e, set)}
+												onDragLeave={(e) => dragEndHandle(e)}
+												onDragEnd={(e) => dragEndHandle(e)}
+												onDragOver={(e) => dragOverHandle(e)}
+												onDrop={(e) => dropHandler(e, set)}
+												draggable={true}
+												key={set.id}
+											>
+												{set.text}
+											</div>
+										))}
 								</div>
 							</div>
 						</div>
